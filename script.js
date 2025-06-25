dc51c43e1b235484327f76fd23d26d1ddda0df36
alidhino <cmr346330@gmail.com>
2025-06-25 20:24:42 +0700

    commit keduabelas manambahkan product

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-//toggle class active
-
-const navbarNav = document.querySelector(".navbar-nav");
-
-//hamburger menu responsif
-document.querySelector("#hamburger-menu").onclick = () => {
-  navbarNav.classList.toggle("active");
+// Toggle class active untuk hamburger menu
+const navbarNav = document.querySelector('.navbar-nav');
+// ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+  navbarNav.classList.toggle('active');
 };
+
 // Toggle class active untuk search form
 const searchForm = document.querySelector('.search-form');
 const searchBox = document.querySelector('#search-box');
@@ -16,11 +15,20 @@ document.querySelector('#search-button').onclick = (e) => {
   e.preventDefault();
 };
 
-//klik diluar sidebar buat ilangin nav
 
-const hamburger = document.querySelector("#hamburger-menu");
-document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-    navbarNav.classList.remove("active");
+
+// Klik di luar elemen
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
+
+
+document.addEventListener('click', function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
   }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove('active');
+  }
+
 });
