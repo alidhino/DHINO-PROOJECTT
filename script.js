116776c3b203763b85dae765c4c0529d86549c2a
alidhino <cmr346330@gmail.com>
2025-06-25 19:31:49 +0700

    commit kesepuluh menambah button search

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ const navbarNav = document.querySelector(".navbar-nav");
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
+// Toggle class active untuk search form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle('active');
+  searchBox.focus();
+  e.preventDefault();
+};
 
 //klik diluar sidebar buat ilangin nav
 
